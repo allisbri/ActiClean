01c372b6ec522b55f53d4c77db306d548a7ddd41
allisbri <allisbri@oregonstate.edu>
2019-04-09 00:09:28 -0400

    fixed and  added removed rows csv creation
    now removes daily/active w/ nan as date
    removes daily/active at beginning or end w/ > 50% error
    replaces all nan with blank cells

--- acti_clean.py
+++ acti_clean.py
@@ -3,8 +3,11 @@ from tkinter import filedialog
 
 # Remove rows where NaN exists in active or daily
     # what about rows where daily or active has NaN, from 'date' on, currently these are deleted
+    # will there be rows where NaN exists only for the checked cell
 # Replace all of the NaN’s with blank cells
+
 # Remove where %invalid is greater than 50 (?)
+
 # For sleep intervals, copy and paste row data (?)
 
 # file_path = 'C:\\Users\\a\\Desktop\\CES_07302018.csv'
@@ -12,6 +15,8 @@ from tkinter import filedialog
 interval_type_col = 4
 onset_latency_col = 22
 percent_invalid_sw_col = 21
+start_date_col = 6
+interval_num_col = 5
 active_interval = 'ACTIVE'
 daily_interval = 'DAILY'
 excluded_interval = 'EXCLUDED'
@@ -32,7 +37,7 @@ def list_string_equals(string_list, index_to_check, string_to_check_for):
 
 def replace_string_in_list(string_list, string_to_replace, string_to_insert):
     c = 0
-    r = row.copy()
+    r = string_list.copy()
     for s in string_list:
         if s == string_to_replace:
             r[c] = string_to_insert
@@ -40,32 +45,134 @@ def replace_string_in_list(string_list, string_to_replace, string_to_insert):
     return r
 
 
+def is_first_in_series(cur):
+    if len(cur) >= (interval_num_col + 1):
+        if cur[interval_num_col] == '1':
+            return True
+    return False
+
+
+def is_last_in_series(cur, next_up, col_num):
+    if len(cur) >= interval_type_col + 1:
+        if not list_string_equals(cur, col_num, next_up[col_num]):
+            return True
+    return False
+
+
+def is_first_or_last_in_series(cur, next_up, col_num):
+    if is_last_in_series(cur, next_up, col_num):
+        return True
+    if is_first_in_series(cur):
+        return True
+    return False
+
+
+def has_start_date(string_list):
+    if len(string_list) < 7:
+        return False
+    if string_list[start_date_col] != 'NaN':
+        return True
+    return False
+
+
+def write_row_without_nan(writer, string_list):
+    new_row = replace_string_in_list(string_list, 'NaN', '')
+    writer.writerow(new_row)
+
+
+def meets_valid_sleep_wake_criteria(cur):
+    if len(cur) >= (percent_invalid_sw_col + 1):
+        if float(cur[percent_invalid_sw_col]) <= 50:
+            return True
+        else:
+            return False
+    else:
+        return True
+
+
+def row_should_be_written(cur, next_up):
+    if list_string_equals(cur, interval_type_col, active_interval) or \
+            list_string_equals(cur, interval_type_col, daily_interval):
+        if not has_start_date(cur):
+            return False
+        if is_first_or_last_in_series(cur, next_up, interval_type_col):
+            if meets_valid_sleep_wake_criteria(cur):
+                return True
+            else:
+                return False
+    return True
+
+
+def last_row_should_be_written(cur):
+    if len(cur) > 0:
+        if list_string_equals(cur, interval_type_col, active_interval) or \
+                list_string_equals(cur, interval_type_col, daily_interval):
+            if not has_start_date(cur):
+                return False
+            if meets_valid_sleep_wake_criteria(cur):
+                return True
+    return True
+
+
+def clean_actigraphy_csv(file_path):
+    count = 0
+    new_file_path = file_path.replace(".csv", "_clean.csv")
+    with open(file_path, 'r', encoding='utf-8-sig') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        with open(new_file_path, 'w', encoding='utf-8-sig', newline='') as new_csv:
+            csv_writer = csv.writer(new_csv)
+            cur = next(csv_reader)
+            for row in csv_reader:
+                next_up = row
+                # write first row
+                if count <= 0:
+                    csv_writer.writerow(cur)
+
+                elif row_should_be_written(cur, next_up):
+                    write_row_without_nan(csv_writer, cur)
+                    print(cur)
+
+                cur = next_up
+                count = count + 1
+
+            # write last row
+            if last_row_should_be_written(cur):
+                write_row_without_nan(csv_writer, cur)
+
+
+def get_removed_rows_csv(file_path):
+    count = 0
+    new_file_path = file_path.replace(".csv", "_removed.csv")
+    with open(file_path, 'r', encoding='utf-8-sig') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        with open(new_file_path, 'w', encoding='utf-8-sig', newline='') as new_csv:
+            csv_writer = csv.writer(new_csv)
+            cur = next(csv_reader)
+            for row in csv_reader:
+                next_up = row
+                # write first row
+                if count <= 0:
+                    csv_writer.writerow(cur)
+
+                elif not row_should_be_written(cur, next_up):
+                    csv_writer.writerow(cur)
+                    print(cur)
+
+                cur = next_up
+                count = count + 1
+
+            # write last row
+            if not last_row_should_be_written(cur):
+                csv_writer.writerow(cur)
+
+
 file_path = get_file_location_from_user_selection()
-new_file_path = file_path.replace(".csv", "_clean.csv")
-with open(file_path, 'r', encoding='utf-8-sig') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    with open(new_file_path, 'w', encoding='utf-8-sig', newline='') as new_csv:
-        csv_writer = csv.writer(new_csv)
-        print('test')
-        for row in csv_reader:
-            print(count)
-            print(row)
-            print('test2')
-            if count <= 0:
-                csv_writer.writerow(row)
-            # exclude rows that have NaN for active interval or daily interval at onset_latency_col
-            elif list_string_equals(row, interval_type_col, active_interval) \
-                    or list_string_equals(row, interval_type_col, daily_interval):
-                    print(row)
-                    if row[onset_latency_col] != 'NaN':
-                        csv_writer.writerow(row)
-            # replace all cases of NaN with blank cells and write rows that have less length
-            elif len(row) <= (percent_invalid_sw_col + 1):
-                new_row = replace_string_in_list(row, 'NaN', '')
-                csv_writer.writerow(new_row)
-            # replace all cases of NaN with blank cells and exclude rows that have less than 50 percent invalid sw
-            elif float(row[percent_invalid_sw_col]) < 50:
-                new_row = replace_string_in_list(row, 'NaN', '')
-                csv_writer.writerow(new_row)
-            count = count + 1
+clean_actigraphy_csv(file_path)
+get_removed_rows_csv(file_path)
+
+
+
+
+
+
 
